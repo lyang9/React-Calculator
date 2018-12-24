14d8de0df95906600288b4466b7e9bad5ff5167c
Linda Yang <lindayang@Lindas-iMac.local>
2018-12-24 04:24:38 -0500

    Created AutoShrinkingText class. Passed in props

---
+++ src/Shrink.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AutoShrinkingText extends Component {
+  render() {
+    return (
+      <div {...this.props}/>
+    );
+  }
+}
+
+export default AutoShrinkingText;
